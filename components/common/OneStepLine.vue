bdd7d4a29da003ab6dd987f9353e015a6a1713bb
Gaidash Artem <artemga@hflabs.ru>
2025-08-27 19:17:17 +0300

    HowWeWork: added content and steps v1

---
+++ components/common/OneStepLine.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="one-step" :class="'step-' + props.step">
+		<SvgoStepsIconStep01 v-if="props.step === 1" />
+		<SvgoStepsIconStep02 v-if="props.step === 2" />
+		<SvgoStepsIconStep03 v-if="props.step === 3" />
+		<SvgoStepsIconStep04 v-if="props.step === 4" />
+
+		<div class="text">{{ props.label }}</div>
+
+		<div class="arrow"></div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	const props = defineProps<{
+		label: string;
+		step: number;
+	}>();
+</script>
+
+<style scoped lang="scss">
+	.one-step {
+		display: flex;
+		grid-gap: 6%;
+
+		width: 100%;
+		padding-top: 18px;
+		border-radius: 14px;
+
+		background-color: var(--color-grey-d);
+
+		svg {
+			width: 120px;
+			height: 100px;
+		}
+
+		.text {
+			font-size: 28px;
+			line-height: 30px;
+		}
+	}
+</style>
--- components/index-page/HowWeWorkBlock.vue
+++ components/index-page/HowWeWorkBlock.vue
@@ -1,29 +1,82 @@
 <template>
-	<div class="how-we-work-block">
-		<div>
-			<div class="how-we-work-block__title">Как мы работаем</div>
-			<div class="how-we-work-block__description">
-				Мы помогаем заведениям работать по стандартам и держать уровень. Проверяем, улучшаем,
-				внедряем. Вы можете выбрать нужную услугу — и получить честную, экспертную обратную связь.
+	<div class="container how-we-work-block">
+		<div class="how-we-work-block__wrapper">
+			<div class="how-we-work-block__content">
+				<div class="how-we-work-block__title text-green">Как мы работаем</div>
+				<div class="how-we-work-block__description">
+					Мы помогаем заведениям работать по стандартам и держать уровень.
+					<br />
+					Проверяем, улучшаем, внедряем. Вы можете выбрать нужную услугу — и получить
+					<br />
+					честную, экспертную обратную связь.
+				</div>
+				<UIAnimatedButton>Получить консультацию</UIAnimatedButton>
 			</div>
-			<UIAnimatedButton>Получить консультацию</UIAnimatedButton>
-		</div>
-		<div class="how-we-work-block__steps">
-			<div class="step step-1">Оставляете заявку</div>
-			<div class="step step-2">Проводим аудит</div>
-			<div class="step step-3">Формируем команду</div>
-			<div class="step step-4">Запускаем проект</div>
+			<div class="how-we-work-block__steps">
+				<OneStepLine :label="'Оставляете заявку'" :step="1" />
+				<OneStepLine :label="'Проводим аудит'" :step="2" />
+				<OneStepLine :label="'Формируем команду'" :step="3" />
+				<OneStepLine :label="'Запускаем проект'" :step="4" />
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 	import UIAnimatedButton from '~/components/buttons/UI-AnimatedButton.vue';
+	import OneStepLine from '~/components/common/OneStepLine.vue';
 </script>
 
 <style scoped lang="scss">
 	.how-we-work-block {
 		width: 100%;
 		height: auto;
+		min-height: 700px;
+
+		&__wrapper {
+			display: flex;
+			grid-gap: 5%;
+
+			width: 100%;
+			height: 100%;
+			padding: 44px 36px;
+			border-radius: 14px;
+
+			color: var(--color-white);
+
+			background-color: var(--color-grey-dd);
+		}
+
+		&__content {
+			display: flex;
+			grid-gap: 28px;
+			flex-direction: column;
+
+			width: 40%;
+			height: auto;
+
+			button {
+				width: 60%;
+			}
+		}
+
+		&__title {
+			width: 100%;
+			font-size: 54px;
+			font-weight: 700;
+			line-height: 56px;
+		}
+
+		&__description {
+			width: 100%;
+			font: var(--font-body-default-medium);
+		}
+
+		&__steps {
+			display: flex;
+			grid-gap: 20px;
+			flex-direction: column;
+			width: 60%;
+		}
 	}
 </style>
